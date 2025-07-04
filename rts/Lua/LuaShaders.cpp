e9a4c1bbc60943f1eb8ab015d24f9ce158a967c4
Slashscreen <SlashScreen@users.noreply.github.com>
2025-07-04 13:55:33 -0700

    rearrange (#2441)

--- rts/Lua/LuaShaders.cpp
+++ rts/Lua/LuaShaders.cpp
@@ -571,13 +571,11 @@ GLint LuaShaders::GetUniformLocation(LuaShaders::Program* p, const char* name)
 /***
  * @class ShaderParams
  * 
- * @field vertex string?
- * 
  * The "Vertex" or vertex-shader is your GLSL-Code as string, its written in a
  * C-Dialect.  This shader is busy deforming the geometry of a unit but it can
  * not create new polygons. Use it for waves, wobbling surfaces etc.
- * 
- * @field tcs string?
+ *
+ * @field vertex string?
  * 
  * The "TCS" or Tesselation Control Shader controls how much tessellation a
  * particular patch gets; it also defines the size of a patch, thus allowing it
@@ -585,21 +583,21 @@ GLint LuaShaders::GetUniformLocation(LuaShaders::Program* p, const char* name)
  * The main purpose of the TCS is to feed the tessellation levels to the
  * Tessellation primitive generator stage, as well as to feed patch data (as its
  * output values) to the Tessellation Evaluation Shader stage.
- * 
- * @field tes string?
+ *
+ * @field tcs string?
  * 
  * The "TES" or Tesselation Evaluation Shader takes the abstract patch generated
  * by the tessellation primitive generation stage, as well as the actual vertex
  * data for the entire patch, and generates a particular vertex from it. Each
  * TES invocation generates a single vertex. It can also take per-patch data
  * provided by the Tessellation Control Shader.
- * 
- * @field geometry string?
+ *
+ * @field tes string?
  * 
  * The "Geometry" or Geometry-shader can create new vertices and vertice-stripes
  * from points.
  * 
- * @field fragment string?
+ * @field geometry string?
  * 
  * The "Fragment" or Fragment-shader (sometimes called pixel-Shader) is post
  * processing the already rendered picture (for example drawing stars on the
@@ -608,6 +606,8 @@ GLint LuaShaders::GetUniformLocation(LuaShaders::Program* p, const char* name)
  * Remember textures are not always 2 dimensional pictures. They can contain
  * information about the depth, or the third value marks areas and the strength
  * at which these are processed.
+ * 
+ * @field fragment string?
  * 
  * @field uniform UniformParam<number>?
  * @field uniformInt UniformParam<integer>?
